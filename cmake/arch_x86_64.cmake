d1fb99f0eadf2b86d38e55cab0c9710d83135c80
Angelo Theodorou <encelo@gmail.com>
2016-07-06 18:23:27 +0100

    Prune default compiler flags as NDK r12 did

--- cmake/arch_x86_64.cmake
+++ cmake/arch_x86_64.cmake
@@ -5,10 +5,11 @@ set(TOOLCHAIN ${TOOLCHAIN_ROOT}/toolchain-${ARCH})
 set(NDK_CFLAGS "-ffunction-sections -funwind-tables -fstack-protector-strong -no-canonical-prefixes")
 set(NDK_LDFLAGS "-no-canonical-prefixes")
 
+set(BUILD_CFLAGS "-g")
  if(CMAKE_BUILD_TYPE STREQUAL "Release")
-	set(BUILD_CFLAGS "-O2 -g -DNDEBUG -fomit-frame-pointer -fstrict-aliasing -funswitch-loops -finline-limit=300")
+	set(BUILD_CFLAGS "${BUILD_CFLAGS} -O2 -DNDEBUG")
 elseif(CMAKE_BUILD_TYPE STREQUAL "Debug")
-	set(BUILD_CFLAGS "-O0 -UNDEBUG -fno-omit-frame-pointer -fno-strict-aliasing")
+	set(BUILD_CFLAGS "${BUILD_CFLAGS} -O0 -UNDEBUG")
  endif()
 
 set(CROSS_CFLAGS "${NDK_CFLAGS} ${BUILD_CFLAGS}")
