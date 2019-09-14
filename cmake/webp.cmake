98e8dc36e63f316ab50ee4e56615b41d28a61fd5
Angelo Theodorou <encelo@gmail.com>
2019-09-14 19:22:37 +0200

    Use CMake to build libwebp
    - Stop using the configure script to allow building the libraries on Windows
    - Add a newer CMakeLists.txt script from upstream to fix Android building issues

--- cmake/webp.cmake
+++ cmake/webp.cmake
@@ -1,30 +1,43 @@
 set(TARGET_WEBP webp)
+set(TARGET_WEBP_STATIC webp_static)
 set(URL_WEBP http://downloads.webmproject.org/releases/webp/libwebp-1.0.2.tar.gz)
 set(URL_MD5_WEBP 02c0c55f1dd8612cd4d462e3409ad35d)
 set(DEST_WEBP ${DESTINATION_PATH}/webp)
 
-set(CPUFEATURES ${NDK_DIR}/sources/android/cpufeatures)
-set(WEBP_CFLAGS "${CROSS_CFLAGS} -I${CPUFEATURES} -DANDROID -DHAVE_MALLOC_H -DHAVE_PTHREAD -DWEBP_USE_THREAD")
-set(WEBP_LDFLAGS "${CROSS_LDLAGS} -lm -L${EP_BASE}/Source/project_${TARGET_WEBP} -lcpufeatures")
+set(WEBP_CMAKE_ARGS
+	-DWEBP_BUILD_ANIM_UTILS=OFF -DWEBP_BUILD_CWEBP=OFF -DWEBP_BUILD_DWEBP=OFF
+	-DWEBP_BUILD_GIF2WEBP=OFF -DWEBP_BUILD_IMG2WEBP=OFF -DWEBP_BUILD_VWEBP=OFF
+	-DWEBP_BUILD_WEBPINFO=OFF -DWEBP_BUILD_WEBPMUX=OFF -DWEBP_BUILD_EXTRAS=OFF)
 
 ExternalProject_Add(project_${TARGET_WEBP}
 	URL ${URL_WEBP}
 	URL_MD5 ${URL_MD5_WEBP}
-	CONFIGURE_COMMAND ${CC} -c -I${CPUFEATURES} ${CPUFEATURES}/cpu-features.c -o ${EP_BASE}/Source/project_${TARGET_WEBP}/cpu-features.o
-		COMMAND ${AR} rcs ${EP_BASE}/Source/project_${TARGET_WEBP}/libcpufeatures.a ${EP_BASE}/Source/project_${TARGET_WEBP}/cpu-features.o
-		COMMAND ./configure ${CONFIGURE_TOOLCHAIN_ARGS_NOFLAGS} CFLAGS=${WEBP_CFLAGS} LDFLAGS=${WEBP_LDFLAGS} --enable-shared=yes --enable-static=yes
-	BUILD_COMMAND ${PARALLEL_MAKE}
-		COMMAND ${AR} r ${EP_BASE}/Source/project_${TARGET_WEBP}/src/.libs/libwebp.a ${EP_BASE}/Source/project_${TARGET_WEBP}/cpu-features.o
-	BUILD_IN_SOURCE 1
-	INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${EP_BASE}/Source/project_${TARGET_WEBP}/src/.libs/libwebp.so ${DEST_WEBP}/${ARCH}/libwebp.so
-		COMMAND ${CMAKE_COMMAND} -E copy_if_different ${EP_BASE}/Source/project_${TARGET_WEBP}/src/.libs/libwebp.a ${DEST_WEBP}/${ARCH}/libwebp.a
-		COMMAND ${CMAKE_COMMAND} -E copy_directory src/webp ${DEST_WEBP}/include/webp
+	PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different ${CMAKE_SOURCE_DIR}/patches/CMakeLists_webp.txt ${EP_BASE}/Source/project_${TARGET_WEBP}/CMakeLists.txt
+	CMAKE_ARGS ${CMAKE_TOOLCHAIN_ARGS} ${WEBP_CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON
+	BUILD_IN_SOURCE 0
+	INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different libwebp.so ${DEST_WEBP}/${ARCH}/libwebp.so
+		COMMAND ${CMAKE_COMMAND} -E copy_directory ${EP_BASE}/Source/project_${TARGET_WEBP}/src/webp ${DEST_WEBP}/include/webp
+		COMMAND ${CMAKE_COMMAND} -E copy_if_different src/webp/config.h ${DEST_WEBP}/include/webp
 		COMMAND ${CMAKE_COMMAND} -E remove ${DEST_WEBP}/include/webp/config.h.in
+)
+
+ExternalProject_Add(project_${TARGET_WEBP_STATIC}
+	DEPENDS project_${TARGET_WEBP}
+	DOWNLOAD_COMMAND ""
+	SOURCE_DIR ${EP_BASE}/Source/project_${TARGET_WEBP}
+	CMAKE_ARGS ${CMAKE_TOOLCHAIN_ARGS} ${WEBP_CMAKE_ARGS} -DBUILD_SHARED_LIBS=OFF
+	BUILD_IN_SOURCE 0
+	INSTALL_COMMAND ${CMAKE_COMMAND} -E copy_if_different libwebp.a ${DEST_WEBP}/${ARCH}/libwebp.a
 )
 
 if(CMAKE_BUILD_TYPE STREQUAL "Release")
 	add_custom_command(TARGET project_${TARGET_WEBP} POST_BUILD
 		COMMAND ${STRIP} ${STRIP_SHARED_ARGS} ${DEST_WEBP}/${ARCH}/libwebp.so
+		COMMENT "Stripping the dynamic WebP library")
+endif()
+
+if(CMAKE_BUILD_TYPE STREQUAL "Release")
+	add_custom_command(TARGET project_${TARGET_WEBP_STATIC} POST_BUILD
 		COMMAND ${STRIP} ${STRIP_STATIC_ARGS} ${DEST_WEBP}/${ARCH}/libwebp.a
-		COMMENT "Stripping WebP libraries")
+		COMMENT "Stripping the static WebP library")
 endif()
